34909a3a57daacd1140aff7480c8d5e29369eab8
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-15 00:36:50 +0600

    Correct shorthand operations and console.log statements
    Fix variable increment and decrement operations, correct console logging, and adjust arithmetic operations.

---
+++ days-8/shorthand.js
@@ -0,0 +1,37 @@
+let x = 10;
+x += 5;
+consol.log(x);
+
+Output: 15
+
+let x = 20;
+x -= 8;
+console.log(x);
+
+Output:12
+
+let x = 4;
+x *= 3;
+consol.log(x);
+Output:12
+
+let x= 20;
+x /= 5 ;
+consolo.log(x);
+Output:4
+
+let x = 7;
+x++;
+consol.log(x);
+Output:8
+
+let x = 7;
+x--;
+consol.log(x);
+Output:6
+
+let price = 100;
+price += 20;
+price *= 2;
+consol.log(price);
+Output:240
