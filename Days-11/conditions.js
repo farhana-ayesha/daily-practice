03e5ce621a13a1f4445fb0759698bec3e9cfc3d6
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-20 03:52:40 +0600

    Create conditions.js

---
+++ Days-11/conditions.js
@@ -0,0 +1,92 @@
+JavaScript Conditions (Beginner Level)
+
+1. Print "Greater" if a number is greater than 10.
+
+let num = 15;
+
+if (num > 10) {
+    console.log("Greater");
+}
+
+---
+
+2. Print "Positive" if a number is positive.
+
+let num = 7;
+
+if (num > 0) {
+    console.log("Positive");
+}
+---
+
+3. Print "Even" if a number is even.
+
+let num = 8;
+
+if (num % 2 === 0) {
+    console.log("Even");
+}
+---
+
+4. Print "Correct" if a number is equal to 100.
+
+let num = 100;
+
+if (num === 100) {
+    console.log("Correct");
+}
+---
+
+5. Print "Eligible to vote" if age is 18 or above.
+
+let age = 20;
+
+if (age >= 18) {
+    console.log("Eligible to vote");
+}
+---
+
+6. Print "Zero" if a number is 0.
+
+let num = 0;
+
+if (num === 0) {
+    console.log("Zero");
+}
+---
+
+7. Print "Correct Language" if the language is "JavaScript".
+
+let language = "JavaScript";
+
+if (language === "JavaScript") {
+    console.log("Correct Language");
+}
+---
+
+8. Print "Small Number" if a number is less than 50.
+
+let num = 25;
+
+if (num < 50) {
+    console.log("Small Number");
+}
+---
+
+9. Print "Success" if a boolean value is true.
+
+let isSuccess = true;
+
+if (isSuccess) {
+    console.log("Success");
+}
+---
+
+10. Print "Divisible by 5" if a number is divisible by 5.
+
+let num = 25;
+
+if (num % 5 === 0) {
+    console.log("Divisible by 5");
+}
+
