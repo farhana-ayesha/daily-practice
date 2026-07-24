83e3deee379212e79e72e9a92247441b75c8f953
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-24 15:45:30 +0600

    Add string manipulation examples in strings.js

---
+++ Days -13/strings.js
@@ -0,0 +1,25 @@
+১. "Programming Hero" এর length বের করো।
+
+const name = 'Programming Hero';
+console.log(name.length);
+
+২. "javascript" কে বড় হাতের অক্ষরে প্রিন্ট করো।
+const name= "javascript";
+console.log(name.toUpperCase());
+
+৩. "HELLO" কে ছোট হাতের অক্ষরে প্রিন্ট করো।
+const text ="HELLO"
+console.log(text.toLowerCase());
+
+৪. "I love JavaScript"-এ "Java" আছে কিনা দেখো।
+const texts = "I love JavaScript";
+console.log(texts.includes("Java"));
+
+৫. "Farhana Ayesha" থেকে "Farhana" অংশ slice() দিয়ে বের করো।
+const name= "Farhana Ayesha";
+console.log(name.slice(0,7));
+
+৬. " apple,banana,mango " থেকে trim() এবং split() ব্যবহার করে একটি Array তৈরি করো।
+
+const fruits = " apple, banana, mango ";
+console.log(fruits.trim().split(", "));
