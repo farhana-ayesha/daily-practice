ffbc18c360366833ea5f20939f151284f494dec5
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-26 17:11:15 +0600

    Create prblm.js

---
+++ Days -14/prblm.js
@@ -0,0 +1,13 @@
+// Input: an array of numbers
+// Output: array with only the numbers greater than 10
+// Returns: an array
+
+function keepNumbersAboveTen(numbers) {
+  let result = [];
+  // TODO: loop এবং শুধু 10-এর চেয়ে বড় number গুলো push করো
+
+  return result;
+}
+
+console.log(keepNumbersAboveTen([5, 12, 8, 20, 3, 15])); 
+// Expected: [12, 20, 15]
