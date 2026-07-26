52ee2b7b1398c3a30edf2dbc9631b982719d3e98
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-26 17:20:30 +0600

    Add function to count negative numbers in array

---
+++ Days -14/problem.js
@@ -0,0 +1,17 @@
+
+
+// Input: an array of numbers
+// Output: how many numbers are negative
+// Returns: a number
+
+function countNegatives(numbers) {
+  let count = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] < 0) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countNegatives([3, -5, 8, -1, -9])); // 3
