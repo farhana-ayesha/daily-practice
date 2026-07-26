d612b68e21a3adbc8f80f95542cf33eeccde84d0
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-26 17:16:48 +0600

    Add function to list values from an object

--- Days -14/prblm.js
+++ Days -14/prblm.js
@@ -13,3 +13,19 @@ function keepNumbersAboveTen(numbers) {
 
 console.log(keepNumbersAboveTen([5, 12, 8, 20, 3, 15])); 
 // Expected: [12, 20, 15]
+___________________________
+
+
+function listValues(obj) {
+  let values = [];
+  for (let key in obj) {
+    values.push(obj[key]);
+  }
+  return values;
+}
+
+console.log(listValues({ name: "Sam", age: 25, city: "Dhaka" }));
+// ["Sam", 25, "Dhaka"]
+
+
+
