a7b2c269a792473f307317d538c0133327898aae
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-27 13:34:20 +0600

    Explain dot vs bracket notation in comments
    Added comments explaining the difference between dot notation and bracket notation in JavaScript.

--- Days -16/bracket-notation.js
+++ Days -16/bracket-notation.js
@@ -12,3 +12,36 @@ output is
 Farhana
 29
 Khulna
+
+
+/*
+Dot আর Bracket-এর পার্থক্য কী?
+দুটোই একই value দেয়।
+person.name
+এবং
+person["name"]
+
+✅ দুটোই Output:
+Farhana
+
+তাহলে Bracket Notation কেন আছে?
+
+ধরি,
+let key = "age";
+এখন যদি লিখি:
+console.log(person[key]);
+JavaScript আগে key-এর value বের করবে।
+key = "age"
+তারপর করবে:
+person["age"]
+Output:
+29
+
+এটাই Bracket Notation-এর আসল শক্তি।
+Dot দিয়ে এটা করা যায় না
+console.log(person.key);
+Output:
+undefined
+কারণ JavaScript এখানে "key" নামে property খুঁজবে, "age" না।
+
+*/
