2e1535ed1f0df83f3ee8ad4f50c9aa7e1743dabf
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-27 13:29:32 +0600

    Add object dot notation examples for book and car

---
+++ Days -16/object-dot-notation.js
@@ -0,0 +1,18 @@
+let book = {
+title: "JavaScript",
+author: "Farhana",
+pages: 250
+};
+
+console.log(book.title); // JavaScript
+console.log(book.author); // Farhana
+console.log(book.pages); // 250
+
+let car = {
+brand: "Toyota",
+color: "White",
+year: 2024
+};
+
+console.log(car.brand); //Toyota
+console.log(car.year); //2024
