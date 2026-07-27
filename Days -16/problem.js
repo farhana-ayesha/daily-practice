e825064ab996dae2f17b7f95934faf0e27b4305f
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-27 12:49:04 +0600

    Update problem.js

--- Days -16/problem.js
+++ Days -16/problem.js
@@ -21,3 +21,13 @@ let unique = [...new Set(fruits)];
 
 console.log(unique);
 Output: [ Apple,Mango, Orange]
+--------------------
+  
+  let numbers = [7, 7, 2, 5, 2, 9];
+
+let unique = [...new Set(numbers)];
+
+console.log(unique.length); // 4 
+console.log(unique[2]);     // 5 
+
+  
