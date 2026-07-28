67334aafa9e96b2a21650db1556a94973d475e85
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-28 22:24:46 +0600

    Add calculatePrice function with default discount

--- Days -18/function.js
+++ Days -18/function.js
@@ -22,3 +22,13 @@ greetUser("Farah"); // Hello, Farah!
 
 console.log(checkEven(7));  // Odd
 console.log(checkEven(10)); // Even
+---------------------------------------
+   //Default Parameter
+
+  function calculatePrice(price, discount = 10) {
+  let finalPrice = price - (price * discount / 100);
+  return finalPrice;
+}
+
+console.log(calculatePrice(1000));     // discount na dile 10% dhore: 900
+console.log(calculatePrice(1000, 20)); // 20% discount: 800
