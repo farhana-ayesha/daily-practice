a19bdae6aca0471aca13a04e2e395aa6d9f65fea
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-28 14:11:01 +0600

    Add user object with keys, values, and entries logging

---
+++ Days 17/objects.js
@@ -0,0 +1,11 @@
+let user = {
+username: "Farhana",
+country: "Bangladesh",
+age: 29
+};
+
+console.log(Object.keys(user)); //['username', 'country', 'age']
+console.log(Object.values(user)); //['Farhana' , 'Bangladesh' , 29]
+console.log(Object.entries(user));[ "username": "Farhana",
+"country": "Bangladesh",
+"age": 29]
