ee3d292ec79d79c5252635e4b10a18dd11c28894
Farhana Ayesha <afrworkid2026@gmail.com>
2026-07-28 22:21:59 +0600

    Add greetUser function for personalized greeting
    Added greetUser function to greet a user by name.

--- Days -18/function.js
+++ Days -18/function.js
@@ -5,3 +5,13 @@ function sayHello() {
 }
 
 sayHello(); // call korle output ashbe
+------------------------------------------
+//Parameter with 
+
+  function greetUser(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greetUser("Farah"); // Hello, Farah!
+
+
