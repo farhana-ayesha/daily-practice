f2cea91bd533421ee4d9a3db8006bb6a7d028a23
Farhana Ayesha <afrworkid2026@gmail.com>
2026-08-03 18:46:17 +0600

    Add arrow function for summing two numbers
    Implemented an arrow function to add two numbers and provided a concise version.

---
+++ Days-21/Arrow-function.js
@@ -0,0 +1,24 @@
+
+
+✅ Problem 1 Solution
+
+দুইটি সংখ্যার যোগফল বের করো।
+
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(10, 20));
+
+Output:
+
+30
+
+আরও ছোট করে লেখা যায়:
+
+const add = (a, b) => a + b;
+
+console.log(add(10, 20));
+
+
+---
