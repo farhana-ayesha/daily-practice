16a6e99de01500ee29275316042feccba360f5a8
Farhana Ayesha <afrworkid2026@gmail.com>
2026-08-07 11:15:30 +0600

    Add ES6.js with variable examples and outputs

---
+++ Days 22/ES6.js
@@ -0,0 +1,24 @@
+let name = "Farhana";
+
+name = "Ayesha";
+
+console.log(name);
+Output: Ayesha
+
+2/ const age = 29;
+age = 30;
+Output: Error
+
+3/ if (true) {
+let x = 100;
+}
+
+console.log(x);
+Output: reference error
+
+4/ const fruits = ["Apple"];
+
+fruits.push("Mango");
+
+console.log(fruits);
+Output:[ Apple,Mango]
